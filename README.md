b16a42fcdd9bf1b9793ed9706e842a3561eb5094
iMaeGoo <mail1st@qq.com>
2022-02-25 15:39:20 +0800

    feat: init pushoo

---
+++ README.md
@@ -0,0 +1,25 @@
+# pushoo.js
+
+## 整合各大消息推送平台服务
+
+## 支持的消息推送平台
+
+- [ ] Qmsg
+- [x] server酱
+- [ ] Push Plus（https://www.pushplus.plus/）
+- [ ] Push Plus（https://pushplus.hxtrip.com/）
+- [ ] 钉钉
+- [ ] 企业微信
+- [ ] Telegram
+- [ ] Bark
+- [ ] 阿里云短信
+- [ ] 腾讯云短信
+- [ ] Discord
+- [ ] iGot - https://push.hellyw.com/
+- [ ] Webhook
+
+## 使用方法
+
+npm install pushoo
+pnpm install pushoo
+yarn add pushoo
---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import axios from 'axios'
+
+interface CommonOptions {
+  token: string
+  title?: string
+  content: string
+  contentType?: string
+}
+
+async function notice(channel: string, options: CommonOptions) {
+  try {
+    let data: any
+    switch (channel) {
+      case 'ServerChain':
+        data = await noticeServerChan(options)
+        break
+      case 'PushPlusHxtrip':
+        data = await noticePushPlusHxtrip(options)
+        break
+      default:
+        throw new Error('not supported')
+    }
+    console.log(`[PUSHOO] Send to <${channel}> result:`, data)
+    return data
+  } catch (e) {
+    console.error('[PUSHOO] Got error:', e)
+    return { error: e }
+  }
+}
+
+/**
+ * https://sct.ftqq.com/
+ */
+async function noticeServerChan(options: CommonOptions) {
+  checkParameters(options, ['token', 'content'])
+  let url: string
+  let param: URLSearchParams
+  if (options.token.substring(0, 3).toLowerCase() === 'sct') {
+    url = 'https://sctapi.ftqq.com'
+    param = new URLSearchParams({
+      title: options.title || options.content,
+      desp: options.content
+    })
+  } else {
+    url = 'https://sc.ftqq.com'
+    param = new URLSearchParams({
+      text: options.title || options.content,
+      desp: options.content
+    })
+  }
+  const response = await axios.post(`${url}/${options.token}.send`, param.toString(), {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  })
+  return response.data
+}
+
+/**
+ * https://pushplus.hxtrip.com/
+ */
+async function noticePushPlusHxtrip(options: CommonOptions) {
+  checkParameters(options, ['token', 'content'])
+  const ppApiUrl = 'http://pushplus.hxtrip.com/send'
+  const ppApiParam = {
+    token: options.token,
+    title: options.title || options.content,
+    content: options.content
+  }
+  const response = await axios.post(ppApiUrl, ppApiParam)
+  return response.data
+}
+
+function checkParameters(options: CommonOptions, requires: string[] = []) {
+  for (const require of requires) {
+    if (!options[require]) {
+      throw new Error(`${require} is required`)
+    }
+  }
+}
+
+export {
+  notice,
+  noticeServerChan,
+  noticePushPlusHxtrip
+}
